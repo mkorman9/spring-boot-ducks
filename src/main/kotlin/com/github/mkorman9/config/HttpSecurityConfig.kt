1de3acff3c5bd5cfce5e63f2e3c112b624741e18
Michal Korman <michal.korman@icloud.com>
2024-03-13 19:53:06 +0100

    configure cors

--- src/main/kotlin/com/github/mkorman9/config/HttpSecurityConfig.kt
+++ src/main/kotlin/com/github/mkorman9/config/HttpSecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.github.mkorman9.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
@@ -7,15 +8,24 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableMethodSecurity
 class HttpSecurityConfig {
     @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+    fun securityFilterChain(
+        http: HttpSecurity,
+        corsConfigurationSource: CorsConfigurationSource
+    ): SecurityFilterChain {
         return http
+            .csrf { csrf ->
+                csrf.disable()
+            }
             .cors { cors ->
-                cors.disable()
+                cors.configurationSource(corsConfigurationSource)
             }
             .authorizeHttpRequests { auth ->
                 auth.anyRequest().permitAll()
@@ -27,5 +37,14 @@ class HttpSecurityConfig {
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             }
             .build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(@Value("\${cors.origin}") corsOrigin: String): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf(corsOrigin)
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
