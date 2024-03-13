a0bc52e92601bd2198a4b9367accb670b45d4352
Michal Korman <michal.korman@icloud.com>
2024-03-13 10:43:55 +0100

    add logging

--- src/main/kotlin/com/github/mkorman9/DuckController.kt
+++ src/main/kotlin/com/github/mkorman9/DuckController.kt
@@ -1,6 +1,7 @@
 package com.github.mkorman9
 
 import jakarta.validation.Valid
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
+import java.security.Principal
 import java.util.UUID
+
+private val log = LoggerFactory.getLogger(DuckController::class.java)
 
 @RestController
 @RequestMapping("/api/ducks")
@@ -30,7 +34,11 @@ class DuckController(
 
     @PostMapping
     @PreAuthorize("isAuthenticated()")
-    fun addDuck(@RequestBody @Valid payload: DuckAddPayload): UUID {
+    fun addDuck(
+        @RequestBody @Valid payload: DuckAddPayload,
+        principal: Principal
+    ): UUID {
+        log.info("User ${principal.name} is adding a duck")
         return duckService.addDuck(payload)
     }
 }
